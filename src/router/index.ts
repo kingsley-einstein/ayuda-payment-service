8eeffad04f9aa61c791f359e4a815bdcced65313
Kingsley Victor <javaprodigy56@gmail.com>
2020-05-31 21:23:55 +0100

    new commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,7 +7,7 @@ router.post("/payment/create", PaymentController.create);
 router.post("/payment/initialize", PaymentController.initializePayment);
 router.post("/transfer/recipient", PaymentController.createTransferRecipient);
 router.post("/transfer/initialize", PaymentController.initializeTransfer);
-router.post("/transfer/finalize", PaymentController.finalizeTransfer);
+// router.post("/transfer/finalize", PaymentController.finalizeTransfer);
 
 router.get("/payment/find", PaymentController.findByReference);
 router.get("/payment/verify", PaymentController.verifyPayment);
