43dce525b86c4235752051fde8f75f9e17909d11
Kingsley Victor <javaprodigy56@gmail.com>
2020-06-03 03:13:09 +0100

    New commit

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -3,6 +3,7 @@ export const cors = (origin: string) => {
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
+    req.header("Host", process.env.NODE_ENV === "production" ? process.env.HOST : "");
     next();
   }
 }
