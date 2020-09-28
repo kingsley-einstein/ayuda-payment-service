ba03e2fcfe93e6ce42c3d2ee65a31902834a2d34
Kingsley Victor <javaprodigy56@gmail.com>
2020-09-28 14:51:39 +0100

    New commit

--- src/middlewares/referral.ts
+++ src/middlewares/referral.ts
@@ -1,17 +1,23 @@
 import rp from "request-promise";
 import env from "../env";
 
+const referralAPI = env.referral_service + "/api/v1/owned"
+
 export const referral = () => {
   return async (req: any, res: any, next: any) => {
     const { headers } = req;
-    const referralAPI = env.referral_service + "/api/v1/owned"
-    console.log(referralAPI);
-    const referralResponse = await rp({ uri: referralAPI, headers, method: "GET", simple: false, json: true, resolveWithFullResponse: true });
+    // console.log(referralAPI);
+    const referralResponse = await rp.get(referralAPI, {
+     json: true,
+     simple: false,
+     resolveWithFullResponse: true,
+     headers
+    });
     if (referralResponse.statusCode >= 400) {
       return res.status(referralResponse.statusCode).json({
         code: referralResponse.statusCode,
         response: referralResponse.body.response || referralResponse.body
-      })
+      });
     }
     req.referral = referralResponse.body.response;
     next();
